d62f35647fc7c664f89bf6dce6bb4e831060c869
Guille <cartogreen@gmail.com>
2016-04-08 18:44:34 +0200

    ""

--- src/es/upm/dit/isst/glucosa/dao/GlucosaDAOImpl.java
+++ src/es/upm/dit/isst/glucosa/dao/GlucosaDAOImpl.java
@@ -69,7 +69,7 @@ public class GlucosaDAOImpl implements GlucosaDAO{
 		EntityManager em = EMFService.get().createEntityManager();
 		Query q = em.createQuery("select m from Usuario m");
 		try{
-			ArrayList<Usuario> lista = (ArrayList<Usuario>) q.getResultList();
+			ArrayList<Usuario> lista = (ArrayList<Usuario>)q.getResultList();
 		}catch(Exception e){
 			System.out.println("no hay nadie");
 		}
