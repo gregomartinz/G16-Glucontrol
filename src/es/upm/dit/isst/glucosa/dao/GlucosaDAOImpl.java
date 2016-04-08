8c9a3ad7d580df5f6e6044b9c43272221ad97486
Guille <cartogreen@gmail.com>
2016-04-08 10:17:46 +0200

    Dao

--- src/es/upm/dit/isst/glucosa/dao/GlucosaDAOImpl.java
+++ src/es/upm/dit/isst/glucosa/dao/GlucosaDAOImpl.java
@@ -2,46 +2,84 @@ package es.upm.dit.isst.glucosa.dao;
 
 import java.util.ArrayList;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import es.dit.upm.isst.t4.dao.EMFService;
+
 import es.upm.dit.isst.glucosa.model.Usuario;
 
 public class GlucosaDAOImpl implements GlucosaDAO{
 
-	public GlucosaDAOImpl() {
-		// TODO Auto-generated constructor stub
+	private static GlucosaDAOImpl instance;
+	private GlucosaDAOImpl () {
+	}
+	public static GlucosaDAOImpl getInstance() {
+		if (instance == null)
+			instance = new GlucosaDAOImpl();
+		return instance;
 	}
 
 	@Override
 	public void create(String dni, String correo, String nombre, String genero,
 			String fechaDiagnostico, String fechaNacimiento,
 			ArrayList<String> datos) {
-		
-		
-		// TODO Auto-generated method stub
-		
+		EntityManager em = EMFService.get().createEntityManager();
+		Usuario user = new Usuario(dni, correo, nombre, genero, fechaDiagnostico, fechaNacimiento, datos);
+		em.persist(user);
+		em.close();
 	}
 
 	@Override
-	public Usuario read(String dni) {
-		// TODO Auto-generated method stub
-		return null;
+	public Usuario readDni(String dni) {
+		Usuario user;
+		EntityManager em = EMFService.get().createEntityManager();
+		Query q = em.createQuery("select m from Usuario m where m.dni = :dni");
+		q.setParameter("dni", dni);
+		try{
+			user = (Usuario)q.getSingleResult();
+		}catch(Exception e){
+			System.out.pritnln("no hay nadie con ese dni");
+		}
+		em.close();
+		return user;
 	}
 	
 	@Override
-	public ArrayList<Usuario> readGenero(String genero){
-		
-		return null;
+	public ArrayList<Usuario> readGenero(String genero) {
+		ArrayList<Usuario> lista = new ArrayList<Usuario>();
+		EntityManager em = EMFService.get().createEntityManager();
+		Query q = em.createQuery("select m from Usuario m where m.genero = :genero");
+		q.setParameter("genero", genero);
+		try{
+			lista = (ArrayList<Usuario>)q.getResultList();
+		}catch(Exception e){
+			System.out.println("no hay nadie de ese genero");
+		}
+		em.close();
+		return lista;
 	}
 
 	@Override
-	public void update() {
-		// TODO Auto-generated method stub
-		
+	public void update(Usuario user) {
+		EntityManager em = EMFService.get().createEntityManager();
+		try{
+			em.merge(user);
+		}catch(Exception e){
+			System.out.println("no se ha podido actualizar");
+		}
+		em.close();
 	}
 
 	@Override
 	public void delete(Usuario user) {
-		// TODO Auto-generated method stub
-		
+		EntityManager em = EMFService.get().createEntityManager();
+		try{
+			em.remove(user);
+		}catch(Exception e){
+			System.out.println("no se ha podido borrar");
+		}
+		em.close();		
 	}
 
 }
