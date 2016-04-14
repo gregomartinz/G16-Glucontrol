a6940195f450ce1a1bc040992d1dac0bfe9a7264
Guille <cartogreen@gmail.com>
2016-04-14 17:30:17 +0200

    stats json

--- src/es/upm/dit/isst/glucosa/StatsServlet.java
+++ src/es/upm/dit/isst/glucosa/StatsServlet.java
@@ -2,10 +2,15 @@ package es.upm.dit.isst.glucosa;
 
 import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.labs.repackaged.org.json.JSONArray;
+import com.orsoncharts.util.json.parser.JSONParser;
 
 import es.upm.dit.isst.glucosa.dao.GlucosaDAO;
 import es.upm.dit.isst.glucosa.dao.GlucosaDAOImpl;
@@ -25,7 +30,11 @@ public class StatsServlet extends HttpServlet{
 		String [] hora1;
 		String [] hora2;
 		String [] hora3;
-		ArrayList<String> stats = new ArrayList<String>();
+		ArrayList<String> statsFecha = new ArrayList<String>();
+		ArrayList<String> statsMedidas = new ArrayList<String>();
+		ArrayList<String> statsHoras = new ArrayList<String>();
+
+
 		ArrayList<String> aux = dao.readDni((String) req.getSession().getAttribute("dni")).getDatos();
 
 		for (String s : aux) {
@@ -45,20 +54,28 @@ public class StatsServlet extends HttpServlet{
 			m3[0] = hora3[0];
 			hora3[0] = hora3[1];
 			
-			stats.add(fecha[0]);
-			stats.add(hora1[0]);
-			stats.add(m1[0]);
-			stats.add(hora2[0]);
-			stats.add(m2[0]);
-			stats.add(hora3[0]);
-			stats.add(m3[0]);
+			statsFecha.add(fecha[0]);
+			statsHoras.add(hora1[0]);
+			statsMedidas.add(m1[0]);
+			statsHoras.add(hora2[0]);
+			statsMedidas.add(m2[0]);
+			statsHoras.add(hora3[0]);
+			statsMedidas.add(m3[0]);
 		}
 		
-		resp.getWriter().print(stats);
-		req.getSession().setAttribute("stats", stats);
-	
-//		RequestDispatcher view = req.getRequestDispatcher("Stats.jsp");
-//		view.forward(req, resp);
+		
+		JSONArray jsfecha = new JSONArray(statsFecha);
+		JSONArray jsmedida = new JSONArray(statsMedidas);
+		JSONArray jshora = new JSONArray(statsHoras);
+
+		
+//		resp.getWriter().print(stats);
+		req.getSession().setAttribute("statsFecha", jsfecha);
+		req.getSession().setAttribute("statsMedidas", jsmedida);
+		req.getSession().setAttribute("statsHoras", jshora);
+
+		RequestDispatcher view = req.getRequestDispatcher("Stats.jsp");
+		view.forward(req, resp);
 	
 	}
 }
